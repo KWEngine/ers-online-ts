6c73fb54eaba67b3ad3887eabcf7f13a0702d823
Lutz Karau <lutz.karau@ers-hameln.de>
2024-01-03 12:42:50 +0100

    resized cblock model

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         nested: resolve(__dirname, 'forum/index.html'),
+        nested: resolve(__dirname, 'b-block/index.html'),
+        nested: resolve(__dirname, 'c-block/index.html'),
+        nested: resolve(__dirname, 'c-block/1og/index.html'),
       },
     },
   },
